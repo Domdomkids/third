8252b1d6b739ea051f0191db94f5c0ada715a4ec
Domdomkids <143125391+Domdomkids@users.noreply.github.com>
2023-08-24 12:50:19 -0700

    Delete db directory

--- db/DB.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.connect(process.env.DBURL ,{useUnifiedTopology:true, useNewUrlParser:true})
-.then(()=> console.log("Database Connected successfully"))
-.catch((err)=>console.log('Error Connection to Database failed',err))
